e0684b38985ca40b4d9ea4ff5962039d10cb7521
BjornB <bjorn.bergmann@betika.com>
2026-05-29 15:40:51 +0200

    feat: export profiles module from barrel

--- extensions/gateframe-provider.ts
+++ extensions/gateframe-provider.ts
@@ -1,2 +1,3 @@
 export { default } from "../.pi/extensions/gateframe-provider/index.ts";
 export * from "../.pi/extensions/gateframe-provider/index.ts";
+export * from "../.pi/extensions/gateframe-provider/profiles.ts";
